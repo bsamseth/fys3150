c12e58ffd67bf4ac49bc390cff515c2786cb6d30
lyderrb <lyderrb@student.matnat.uio.no>
2015-09-11 11:04:40 +0200

    Few tweaks to errorEstimation.py

--- errorEstimation.py
+++ errorEstimation.py
@@ -2,7 +2,7 @@
 import sys
 import numpy as np
 from subprocess import call
-from scitools.std import *
+import matplotlib.pyplot as plt
 
 try:
     N = np.asarray([int(i) for i in sys.argv[1:]])
@@ -10,28 +10,34 @@ except IndexError:
     print "Usage: %s list of N" % sys.argv[0]
     sys.exit(1)
 
-
 eps = np.zeros(len(N))
 s = 0
 for N_ in N:
+    call(["./main.x", "%d" % N_])  # compute and write data
     data = open("data/v_solve_N_%d.dat" % N_, 'r')
     data = data.read()
     data = data.split("\n")
-    v = zeros(N_)
+    v = np.zeros(N_)
+    
+    
+
     for i, line in enumerate(data):
         i -= 1
         if i < 1 or line == "": continue
         v[i] = float(line)
     
-    x = linspace(0,1,N_)
-    x_exact = linspace(0,1,N_)
-    u_exact = x_exact * (exp(-10) - 1) - exp(-10*x_exact)  + 1
-
-    eps[s] = max(np.log10((v-u_exact)/(u_exact+1e-10)))
+    x = np.linspace(0,1,N_)
+    x_exact = np.linspace(0,1,N_)
+    u_exact = x_exact * (np.exp(-10) - 1) - np.exp(-10*x_exact)  + 1
+    eps[s] = max(np.log10(v-u_exact+1e-16)-np.log(u_exact+1e-16))
     s+=1
 
 h_ = 1.0/(N-1)
 print h_, eps
-semilogx(h_, eps, legend=["$eps$"]); title('Error estimation')
-xlabel('$log10(h)$'); ylabel('Max value of $ \epsilon_i$')
-raw_input()
+plt.rc('text', usetex=True)
+plt.semilogx(h_, eps); #label=r'$\epsilon$');
+plt.title('Error estimation')
+plt.xlabel(r'$ \log_{10} h$');
+plt.ylabel(r'Max value of $\epsilon_i$')
+#plt.legend()
+plt.show()
