25be90dbea12e2bddb19b7efbfa6d40f9ae513e6
lyderrb <lyderrb@student.matnat.uio.no>
2015-09-20 17:45:45 +0200

    Testa jacobiMethod.cpp. Fant en error der vi hadde tau*tau i stedet for t*t. Testet for mange forskjellige matriser og sammenlignet med matlab. Virker litt 'tilfeldig' om egenverdiene blir bra eller ikke. Lurer på om implementasjonen er i orden nå, men at algoritmen bare er litt dårlig. Tror vi bør spørre på datalabben. La også til max_Aij (se side 217 i lecture notes).

--- jacobiMethod.cpp
+++ jacobiMethod.cpp
@@ -30,18 +30,26 @@ void find_indexes_of_max(double** A, int n, int* l, int* k){
   for(int i = 0; i < n; i++){
     for(int j = 0; j < n; j++){
       if(i!=j && abs(A[i][j]) > tmp){
-	  *l = i;
-	  *k = j;
-	  tmp = abs(A[i][j]);
+	*k = i;  //k should be the row and l the column
+	*l = j;
+	tmp = abs(A[i][j]);
       }
     }
   }
-  if(k>l){
-    int tmp_l;
-    tmp_l = *l;
-    *l = *k;
-    *k = tmp_l;
+}
+
+double max_Aij(double** A, int n){
+  // simple function for finding the max
+  // off diagonal-element of A
+  double max_ = 0.0;
+  for(int i = 0; i < n; i++){
+    for(int j = 0; j < n; j++){
+      if(i != j && A[i][j]*A[i][j] > max_){
+	max_ = A[i][j]*A[i][j];
+      }
+    }
   }
+  return max_;
 }
 
 void jacobiMethod(double** A, int n, double* lambda, double eps){
@@ -50,27 +58,31 @@ void jacobiMethod(double** A, int n, double* lambda, double eps){
   double tmp_Akk, tmp_All, tmp_Akl, tmp_Ail, tmp_Aik;
   while(off_diagonal_sum(A, n) > eps){
     find_indexes_of_max(A, n, &l, &k);
-    tau = (A[l][l]-A[k][k]) / (2*A[k][l]);
-    t = min(abs(-tau+sqrt(1+tau*tau)), abs(-tau-sqrt(1+tau*tau)));
-    c = 1/sqrt(1+tau*tau);
+    tau = (A[l][l]-A[k][k]) / (2*A[k][l]); //hva hvis A[k][l] = 0?
+    if(tau > 0) {  //slik de gjorde det i lecture-notes
+      t = 1.0/(tau + sqrt(1.0 + tau*tau)); 
+    } else { //ekvivalent med det vi hadde?
+      t = -1.0/(-tau + sqrt(1.0 + tau*tau));
+    }
+    c = 1.0/sqrt(1+t*t); //her hadde vi tau*tau
     s = t*c;
     
     tmp_Akk = A[k][k];
     tmp_All = A[l][l];
     tmp_Akl = A[k][l];
 
-    A[k][k] = tmp_Akk*c*c - 2*tmp_Akl*c*s + tmp_All*s*s;
-    A[l][l] = tmp_All*c*c + 2*tmp_Akl*c*s + tmp_Akk*s*s;
-    A[k][l] = A[l][k] = 0;
+    A[k][k] = tmp_Akk*c*c - 2.0*tmp_Akl*c*s + tmp_All*s*s;
+    A[l][l] = tmp_All*c*c + 2.0*tmp_Akl*c*s + tmp_Akk*s*s;
+    A[k][l] = A[l][k] = 0;//(tmp_Akk-tmp_All)*c*s+tmp_Akl*(c*c-s*s);
     for(int i = 0; i < n; i++){
       if(i!=k && i!=l){
 	tmp_Aik = A[i][k];
 	tmp_Ail = A[i][l];
-	A[i][k] = tmp_Aik*c - tmp_Ail*s;
-	A[i][l] = tmp_Ail*c + tmp_Aik*s;
+	A[i][k] = A[k][i] = tmp_Aik*c - tmp_Ail*s;
+	A[i][l] = A[l][i] = tmp_Ail*c + tmp_Aik*s;
       }
     }
-      
+    counter++;  
   }
   for(int i = 0; i < n; i++){
     lambda[i] = A[i][i];
@@ -79,18 +91,28 @@ void jacobiMethod(double** A, int n, double* lambda, double eps){
 
 int main(int argc, char ** argv){
 
-  double** A = new double*[2];
-  A[0] = new double[2];
-  A[1] = new double[2];
-    
+  //some code for generating a matrix
+  int n = 4;
+  double** A = new double*[n];
+  for(int i=0; i<n; i++){
+    A[i] = new double[n];
+  }
 
-  A[0][0] = 2;
-  A[0][1] = -4;
-  A[1][0] = -1;
-  A[1][1] = -1;  
-  double* lambda = new double[2];
-  double eps = 1e-13;
+  //filling matrix with 'arbitrary' values
+  int counter = 1;
+  for(int i=0; i<n; i++){
+    for(int j=0; j<n; j++){
+      A[i][j] = counter;
+      counter += 2;
+    }
+  }
+  
+  
+  double* lambda = new double[n];
+  double eps = 1e-15;
   int l,k;
-  jacobiMethod(A, 2, lambda, eps);
-  cout << lambda[0] << "  " << lambda[1] << endl;
+  jacobiMethod(A, n, lambda, eps);
+  for(int i = 0; i < n; i++){
+    cout << lambda[i] << "  " << endl;
+  }
 }
