3f7aafd87b992852544dfb214bb0f157fa77fece
lyderrb <lyderrb@student.matnat.uio.no>
2015-10-04 14:51:11 +0200

    Små omformuleringer i rapport.

--- tangensPlot.py
+++ tangensPlot.py
@@ -11,7 +11,7 @@ plt.plot(tau_, np.arctan(t1), label=r'$\theta_1 = tan^{-1}(\tau + \sqrt{1+\tau^2
 plt.plot(tau_, np.arctan(t2), label=r'$\theta_2 = tan^{-1}(\tau - \sqrt{1+\tau^2}$)')
 plt.plot(tau_,ones, 'r--')
 plt.plot(tau_, -ones, 'r--', label=r'$\pm \frac{\pi}{4}$', )
-plt.xlabel(r'$ cos 2 \theta / [ \tau ]$', fontsize='large')
+plt.xlabel(r'$ cot 2 \theta / [ \tau ]$', fontsize='large')
 plt.ylabel(r'Angle, \theta', fontsize='large')
 plt.vlines(0, -2, 2, colors = u'k', linestyles='dashed')# np.pi/2, np.pi/2)
 plt.legend()
