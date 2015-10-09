5fc129d681ec2b5c621f71eebb6d4b85ca6dc63c
lyderrb <lyderrb@student.matnat.uio.no>
2015-10-09 13:57:33 +0200

    La til savefig i plotFunc

--- project3/plotFunc.py
+++ project3/plotFunc.py
@@ -5,5 +5,12 @@ r = np.linspace(0, 5, 1001)
 alpha = 2
 psi = np.exp(-alpha * r)
 
-plt.plot(r,psi)
+
+plt.plot(r,psi); 
+plt.rc('text', usetex=True)
+plt.xlabel(r'$r = r1+r2$', fontsize='large')
+plt.ylabel(r'$\Psi$', fontsize='large')
+plt.title(r'Plot av $\Psi$ som funksjon av r')
+
+plt.savefig('../fig/psiPlot.png')
 plt.show()
