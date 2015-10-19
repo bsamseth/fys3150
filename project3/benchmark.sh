2adb9662b350763044cdf05ab6c6bc311ade384c
Bendik Samseth <b.samseth@gmail.com>
2015-10-19 22:34:41 +0200

    Benchmarks
    Laget skript benchmark.sh som lager mappe benchmarks/
    Følgende struktur blir laget:
    .
    ├── benchmarks
    │   ├── output
    │   │   ├── gauss_laguerre_25.dat
    │   │   ├── gauss_legendre_25.dat
    │   │   ├── smartMonteCarlo_10000000.dat
    │   │   └── uniformmontecarlo_10000000.dat
    │   └── fig
    │       └── psiPlot.png

---
+++ project3/benchmark.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+# Note, if the script fails, a re-run has been effective in solving the problems
+# This script will create the following structure (output of tree)
+# .
+# ├── benchmarks
+# │   ├── output
+# │   │   ├── gauss_laguerre_25.dat
+# │   │   ├── gauss_legendre_25.dat
+# │   │   ├── smartMonteCarlo_10000000.dat
+# │   │   └── uniformmontecarlo_10000000.dat
+# │   └── fig
+# │       └── psiPlot.png
+#
+# These results should correspond to the data and figures referenced
+# in the report, aswell as the benchmarks/
+
+# define name of folders
+PARENT=$(pwd)
+DIR=$PARENT/benchmarks
+SRC=src
+DATA=output
+FIG=fig
+DIRSRC=$PARENT/$SRC
+DIRDATA=$DIR/$DATA
+DIRFIG=$DIR/$FIG
+# create data and fig folder
+echo "Creating folder structure"
+mkdir -p $DIRSRC
+mkdir -p $DIRDATA
+mkdir -p $DIRFIG
+
+cp $DIRSRC/*.cpp $DIR/
+cp $DIRSRC/*.h $DIR
+cp $DIRSRC/*.py $DIR
+cp $DIRSRC/Makefile $DIR
+# enter $DIR, needed because some programs write to relative path
+echo "Entering benchmark directory"
+cd $DIR
+
+# compile all executables
+echo "Compiling source files"
+make
+echo "Compilation complete"
+
+# =================================
+# run programs with selcted output.
+# =================================
+#This should reproduce the results reffered to in the report.
+echo "Running gauss_legendre.x for selected arguments"
+./gauss_legendre.x -2 2 25 > $DATA/gauss_legendre_30.dat
+echo "Running gauss_laguerre.x for selected arguments"
+./gauss_laguerre.x 35 > $DATA/gauss_laguerre_35.dat
+echo "Running uniformmonteCarlo.x for selected arguments"
+./uniformmonteCarlo.x -2 2 1000000 > $DATA/uniformmontecarlo_1000000.dat
+echo "Running smartMonteCarlo.x for selected arguments"
+./smartMonteCarlo.x 1000000 > $DATA/smartMonteCarlo_1000000.dat
+
+# this will genereate all the figures shown in the report (only one)
+echo "Running tangensPlot.py"
+python plotFunc.py
+# plotFunc.py stores the produced fig in ../fig/psiPlot.png
+# so cp this benchmarks/fig/
+cp $PARENT/fig/psiPlot.png $DIRFIG/
+
+echo "Cleaning up"
+rm *.cpp
+rm *.h
+rm *.py
+rm Makefile
+rm *.x
+rm *.o
+
+# exit back to $PARENT
+echo "Exiting benchmark directory"
+cd $PARENT
