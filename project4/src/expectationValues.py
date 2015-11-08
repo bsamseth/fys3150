bb38b4975ab1d80976f53aa0c11c2fbcaaabe99a
lyderrb <lyderrb@student.matnat.uio.no>
2015-11-08 17:56:07 +0100

    Noen omformuleringer i rapport.

--- project4/src/expectationValues.py
+++ project4/src/expectationValues.py
@@ -99,7 +99,9 @@ ax[0].set_ylabel(r"$\sigma_M^2$", size=16)
 ax[1].set_ylabel(r"$\sigma_{|M|}^2$", size=16)
 ax[2].set_ylabel(r"$\langle C_V \rangle$", size=16)
 ax[2].set_xlabel(r"Monte Carlo Cycles")
-ax[0].set_title(r'Variances as functions of Monte Carlo Cycles \\ with $n_spins T0 T1 dT MCcycle_min MCcycle_max MCcycle_N randomizer = %s $' %([float(i) for i in sys.argv[1:-1]])) 
+ax[0].set_title(r'Variances as functions of Monte Carlo Cycles \\' \
+                'with $n_spins T0 T1 dT MCcycle_min MCcycle_max MCcycle_N randomizer = %s $' \
+                %([float(i) for i in sys.argv[1:-1]])) 
 ax[len(ax)/2].legend(loc='center left', bbox_to_anchor=(1, 0.5)) 
 plt.savefig("../fig/variances%s.png" % randomName)
 
@@ -108,7 +110,9 @@ for i in range(T_N):
     ax.semilogx(MCcycles, conf_N[i,:]/MCcycles, label=r"$T = %g$" % T_array[i])
 ax.set_xlabel("Monte Carlo Cycles")
 ax.set_ylabel("Configurations")
-ax.set_title(r'Number of accepted configurations per Monte Carlo Cycle \\ with $n_spins T0 T1 dT MCcycle_min MCcycle_max MCcycle_N randomizer = %s$' %([float(i) for i in sys.argv[1:-1]])) 
+ax.set_title(r'Number of accepted configurations per Monte Carlo Cycle \\'
+             'with $n_spins T0 T1 dT MCcycle_min MCcycle_max MCcycle_N randomizer = %s$' \
+             %([float(i) for i in sys.argv[1:-1]])) 
 ax.legend()
 plt.savefig("../fig/configurations%s.png" % randomName)
 plt.show()
