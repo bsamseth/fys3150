a5c0afa9f609ad9ab1fee0359cd51c40b99889b9
Bendik Samseth <b.samseth@gmail.com>
2015-12-02 21:45:40 +0100

    methodComp.py burde nå gi fin tabell.
    Eks.:
    |                  Verlet                                     RK4                   |
    |===================================================================================|
    |_________________step, h___________________________________step, h_________________|
    | T=0.30 | (3.4E-01, 000.1) (2.0E-01, 001.2)  |  (1.2E+00, 000.2) (1.6E-02, 002.2)  |
    | T=0.54 | (7.7E-01, 000.2) (3.6E-01, 002.1)  |  (2.2E+00, 000.4) (5.6E-02, 004.1)  |
    | T=0.79 | (1.5E+00, 000.4) (4.9E-01, 003.2)  |  (2.6E+00, 000.6) (2.4E-01, 006.1)  |
    | T=1.03 | (6.0E+00, 000.4) (6.2E-01, 004.1)  |  (2.7E+00, 000.8) (1.6E-01, 008.3)  |
    | T=1.28 | (6.8E+00, 000.5) (7.9E-01, 005.8)  |  (3.0E+00, 001.0) (6.0E-01, 010.2)  |
    | T=1.52 | (6.8E+00, 000.6) (8.0E-01, 006.7)  |  (3.3E+00, 001.2) (6.6E-01, 012.3)  |
    | T=1.77 | (6.8E+00, 000.8) (7.9E-01, 007.4)  |  (3.3E+00, 001.4) (7.5E-01, 014.0)  |
    | T=2.01 | (6.8E+00, 000.8) (8.0E-01, 008.7)  |  (3.4E+00, 002.0) (7.4E-01, 017.3)  |
    | T=2.26 | (6.8E+00, 000.9) (8.0E-01, 009.1)  |  (3.4E+00, 001.8) (7.4E-01, 017.7)  |
    | T=2.50 | (6.8E+00, 001.1) (9.1E-01, 010.3)  |  (3.4E+00, 002.0) (6.6E-01, 019.7)  |

--- project5/src/methodComp.py
+++ project5/src/methodComp.py
@@ -11,18 +11,29 @@ def center(s, width, sep=" "):
     spaces = sep * lenspace
     return spaces + s + spaces + sep*(chars%2 == 0)
 
+def center2(s1, s2, width):
+    s1 = center(s1,width/2)
+    s2 = center(s2,width/2)
+    return s1+s2
+
+def center(s, width, sep=""):
+    return ('{:%s^%d}' % (sep,width)).format(s)
+
 # os.system("make")
 
 N = 100
-log_h_list = range(-2,-5,-1)
+log_h_list = range(-2,-4,-1)
 t_max_list = np.linspace(0.3, 2.5, 10)
 
-width = 6+ 2*len(log_h_list)
+width = "| T=0.00 | " + " ".join(["(0.0E-00, 000.0)" for i in range(len(log_h_list))]) + "  |  " \
+        + " ".join(["(0.0E-00, 000.0)" for i in range(len(log_h_list))]) + "  |"
+width = len(width)
+print "|%s%s|" % (center("Verlet", width/2), center("RK4", width/2 -1))
+print "|" + "="*(width-2) + "|"
+print "|%s%s|" % (center("step, h", width/2, sep="_"), center("step, h", width/2 -1, sep="_"))
+# print ("|   | " + " ".join([str(h) for h in log_h_list]))*2
 
-print "|   %s%s|" % (center("Verlet", width), center("RK4", width))
-print "|   " + "="*(2*width) + "|"
-print "|   %s%s|" % (center("step, h", width, sep="_"), center("step, h", width, sep="_"))
-print ("|   | " + " ".join([str(h) for h in log_h_list]))*2
+
 # sys.exit(0)
 for t_max in t_max_list:
     print "| T=%.2f |" % t_max,
@@ -34,6 +45,6 @@ for t_max in t_max_list:
             output=proc.communicate()[0]
             t1 = time.time()
             error = float(output.split()[-1])
-            print "(%.1E, %.1f)" % (abs(error), t1-t0),
+            print "(%.1E, %05.1f)" % (abs(error), t1-t0),
         print " | ",
     print 
