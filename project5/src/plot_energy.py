439b95f6c77a266acf977fba2ea275b2457156ea
lyderrb <lyderrb@student.matnat.uio.no>
2015-12-10 18:23:32 +0100

    Rapport og pynting av plot. Vanskelig å flotte figurer innad i tospaltenotatene. Må se på hva vi gjør med det i morgen.

--- project5/src/plot_energy.py
+++ project5/src/plot_energy.py
@@ -12,7 +12,25 @@ except Exception:
     print "Usage: %s datafile" % sys.argv[0]
     sys.exit(1)
 
+
+logh, Tmax, N, check_ =  [float(datafile[:-4].split('_')[i+3]) for i in range(0,4)]
+
+if check_:
+    s = 'Her fjerner vi utskytne partikler'
+else:
+    s = 'Her fjerner vi \_ikke\_ utskytne partikler'
+
+
 data = loadtxt(datafile)
-plt.plot(data[:,0])
+
+t_array = [Tmax*i/1000.0 for i in range(len(data[:,0]))]
+plt.rc('font', **{'family': 'serif', 'serif': ['Computer Modern']})
+plt.rc('text', usetex = True)
+plt.plot(t_array, data[:,0]/N)
+plt.title(r'Energi som funksjon av tiden', fontsize='large')
+plt.xlabel(r'Tid  [$\tau_{crunch}$] \\ \\' \
+           + r'Parametre: $h = %g, T_{max} = %.1f, N = %d$ \\' \
+           %(exp(logh), Tmax, N) + s, fontsize='large')
+plt.ylabel(r'Energi per partikkel', fontsize='large')
 plt.show()
 
